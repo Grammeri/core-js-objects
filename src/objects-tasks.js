b7eed6b3b0c3ef26e7ca4c7947d311f45089e247
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2024-03-04 04:19:14 -0600

    feat: resolve makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -140,8 +140,22 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  // Создаем массив достаточно большого размера, чтобы вместить все буквы.
+  // Используем reduce для прохода по каждой букве и ее позициям.
+  const wordArray = Object.entries(lettersObject).reduce(
+    (acc, [letter, positions]) => {
+      // Для каждой позиции добавляем букву в массив.
+      positions.forEach((position) => {
+        acc[position] = letter;
+      });
+      return acc;
+    },
+    []
+  );
+
+  // Преобразуем массив обратно в строку и возвращаем результат.
+  return wordArray.join('');
 }
 
 /**
